192f6e682d9313636ecf90f385aea11ba6c00348
wangkun0127 <1932819302@qq.com>
2017-07-12 09:13:14 +0800

    C语言学习第二天

---
+++ c02/printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int id = 123;
+	float score = 99.9;
+	int age = 20;
+
+	printf("要输出的文本:");
+	printf("age: %d,score: %.2f,id:%d\n",age,score,id);
+	return 0;
+
+
+}
